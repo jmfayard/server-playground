9a70560e9b35b72b852dea50a42e424fbb3b90d2
Jean-Michel Fayard <jmfayard@gmail.com>
2021-06-12 20:04:28 +0200

    Upgrade Kotlin

--- spring-fu/settings.gradle.kts
+++ spring-fu/settings.gradle.kts
@@ -1,14 +1,11 @@
 pluginManagement {
     repositories {
         gradlePluginPortal()
-        maven("https://s01.oss.sonatype.org/content/repositories/snapshots")
     }
 }
 plugins {
     // See https://jmfayard.github.io/refreshVersions
-    id("de.fayard.refreshVersions") version "0.10.1-SNAPSHOT"
-////                            # available:"0.10.1"
-////                            # available:"0.10.2-SNAPSHOT"
+    id("de.fayard.refreshVersions") version "0.10.1"
 }
 
 rootProject.name = "spring-fu"
