d9c38d8f4f41d57d29bd4d30ece194f1734f7f19
Jean-Michel Fayard <jmfayard@gmail.com>
2021-06-14 16:25:51 +0200

    Try enabling CORS :(

---
+++ graphql-gateway/src/main/kotlin/dev/jmfayard/factsdemo/config/WebConfig.kt
@@ -0,0 +1,15 @@
+package dev.jmfayard.factsdemo.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.reactive.config.CorsRegistry
+import org.springframework.web.reactive.config.WebFluxConfigurer
+
+@Configuration
+class WebConfig : WebFluxConfigurer {
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowCredentials(true)
+            .allowedMethods("GET", "POST", "PUT")
+    }
+}
