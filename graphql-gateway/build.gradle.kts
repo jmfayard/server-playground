e2c74e6efe93674c0e382bef940d36a190145d80
Jean-Michel Fayard <jmfayard@gmail.com>
2021-06-14 16:59:26 +0200

    Show dependencies

--- graphql-gateway/build.gradle.kts
+++ graphql-gateway/build.gradle.kts
@@ -17,10 +17,10 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-webflux")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.springframework.boot:spring-boot-starter-webflux")
 	implementation("com.graphql-java-kickstart:graphql-java-tools:_")
 	implementation("com.graphql-java-kickstart:graphql-spring-boot-starter:_")
 	implementation("com.graphql-java-kickstart:graphiql-spring-boot-starter:_")
