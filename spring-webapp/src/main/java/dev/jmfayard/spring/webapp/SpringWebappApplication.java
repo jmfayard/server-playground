f67337f93dfb524ed80ed06719ba1608ba58b9fe
Jean-Michel Fayard <jmfayard@gmail.com>
2021-07-24 13:09:05 +0200

    Add spring-webapp from Spring Initializer

---
+++ spring-webapp/src/main/java/dev/jmfayard/spring/webapp/SpringWebappApplication.java
@@ -0,0 +1,13 @@
+package dev.jmfayard.spring.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebappApplication.class, args);
+    }
+
+}
---
+++ spring-webapp/src/test/java/dev/jmfayard/spring/webapp/SpringWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jmfayard.spring.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
