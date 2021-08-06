21794b26f85183e52955dde25c2f1629113fbfdb
Jean-Michel Fayard <jmfayard@gmail.com>
2021-08-06 16:02:50 +0200

    Setup with refreshVersions

--- ktor-testing/build.gradle.kts
+++ ktor-testing/build.gradle.kts
@@ -1,11 +1,7 @@
-val ktor_version: String by project
-val kotlin_version: String by project
-val logback_version: String by project
-
 plugins {
     application
-    kotlin("jvm") version "1.5.21"
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.5.21"
+    kotlin("jvm")
+    id("org.jetbrains.kotlin.plugin.serialization")
 }
 
 group = "jmfayard.dev"
@@ -19,13 +15,13 @@ repositories {
 }
 
 dependencies {
-    implementation("io.ktor:ktor-server-core:$ktor_version")
-    implementation("io.ktor:ktor-locations:$ktor_version")
-    implementation("io.ktor:ktor-server-host-common:$ktor_version")
-    implementation("io.ktor:ktor-mustache:$ktor_version")
-    implementation("io.ktor:ktor-serialization:$ktor_version")
-    implementation("io.ktor:ktor-server-netty:$ktor_version")
-    implementation("ch.qos.logback:logback-classic:$logback_version")
-    testImplementation("io.ktor:ktor-server-tests:$ktor_version")
-    testImplementation("org.jetbrains.kotlin:kotlin-test:$kotlin_version")
+    implementation("io.ktor:ktor-server-core:_")
+    implementation("io.ktor:ktor-locations:_")
+    implementation("io.ktor:ktor-server-host-common:_")
+    implementation("io.ktor:ktor-mustache:_")
+    implementation("io.ktor:ktor-serialization:_")
+    implementation("io.ktor:ktor-server-netty:_")
+    implementation("ch.qos.logback:logback-classic:_")
+    testImplementation("io.ktor:ktor-server-tests:_")
+    testImplementation("org.jetbrains.kotlin:kotlin-test:_")
 }
--- ktor-testing/settings.gradle.kts
+++ ktor-testing/settings.gradle.kts
@@ -1,1 +1,12 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        maven("https://s01.oss.sonatype.org/content/repositories/snapshots")
+    }
+}
+plugins {
+    // See https://jmfayard.github.io/refreshVersions
+    id("de.fayard.refreshVersions") version "0.11.1-SNAPSHOT"
+}
+
 rootProject.name = "ktor-testing"
